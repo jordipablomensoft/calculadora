240522f72e96da99fcbbbf65e6b7960c0c9dae64
Jordi <jordi.pablo@mensoftlabs.com>
2024-09-24 18:10:09 +0200

    calculadora html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'calculator',
+    loadComponent: () =>
+      import(
+        './calculator/components/calculator-view/calculator-view.component'
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: 'calculator',
+  },
+];
